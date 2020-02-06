63b22067f5248d631931ee30aa18f57125cba8ae
coolinovo <3104272@qq.com>
2020-02-06 16:12:33 +0800

    添加 less 支持

--- config-overrides.js
+++ config-overrides.js
@@ -3,12 +3,19 @@
  * 还需要修改 package.json 的 scripts 选项
  * 从 customize-cra 引入相关的方法
 */
-const { override, fixBabelImports } = require('customize-cra')
+const {
+  override,
+  fixBabelImports,
+  addLessLoader
+} = require('customize-cra')
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css'
+  }),
+  addLessLoader({
+    javascriptEnabled: true
   })
 )
